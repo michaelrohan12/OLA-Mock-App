faf2abe9bbdaf8d43ceddbec64cc763e9fbe4ec4
michaelrscodes@gmail.com <michaelrscodes@gmail.com>
2023-02-07 23:27:00 +0530

    Header file with local variables used in the main driver code

---
+++ locals.h
@@ -0,0 +1,52 @@
+#ifndef LOCALS_H_INCLUDED
+#define LOCALS_H_INCLUDED
+
+//All the local variables that will be used in the main function
+int choice1;
+
+int choice2;
+
+int choice3;
+
+int signup;
+
+int status = 0;
+
+int flag1=1;
+
+int flag2;
+
+int flag3;
+
+int choice4;
+
+char mobile_no[10];
+
+char email_id[40];
+
+char password[40];
+
+char temp[6];
+
+char* pickup;
+
+char* destination;
+
+float cost = 0.0;
+
+char vehicle_type[40];
+
+float fare;
+
+int fare_choice;
+
+float dis;
+
+char* final_dest;
+
+char coupon_temp[40];
+
+float amt;
+
+
+#endif // LOCALS_H_INCLUDED
